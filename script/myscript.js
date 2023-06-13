4a97cc192f0c0e32fe089daf5615b08d245d5365
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-06-13 15:37:19 +0200

    added vue.js

---
+++ script/myscript.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}).mount('#app')
+
+
+//Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail 
+//generare 10 indirizzi email e stamparli in pagina all'interno di una lista usando Vue e Axios 
+//come visto a lezione.
